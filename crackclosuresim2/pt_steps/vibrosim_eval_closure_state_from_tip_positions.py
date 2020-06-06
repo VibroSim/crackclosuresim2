93d2886c0019f1f0e172bf7c7f3ce7f4ae2eb3d5
Steve Holland <sdh4@iastate.edu>
2020-06-05 20:50:44 -0500

    add nu=0.29 surrogate for fabrikant model; vibrosim_eval_closure_state_from_tip_positions: Assume symmetric COD with Tada/Fabrikant models

--- crackclosuresim2/pt_steps/vibrosim_eval_closure_state_from_tip_positions.py
+++ crackclosuresim2/pt_steps/vibrosim_eval_closure_state_from_tip_positions.py
@@ -101,13 +101,13 @@ def run(_xmldoc,_element,
 
     if dc_crack_model_normal_str=="ModeI_throughcrack_CODformula" and dc_symmetric_cod_bool is None:
         raise ValueError("dc:symmetric_cod must be set to 'true' or 'false' when using ModeI_throughcrack_CODformula crack model")
-    elif dc_crack_model_normal_str=="Tada_ModeI_CircularCrack_along_midline":
+    elif dc_crack_model_normal_str=="Tada_ModeI_CircularCrack_along_midline" and dc_symmetric_cod_bool is None:
         dc_symmetric_cod_bool=True # Tada crack inherently symmetric
         pass
 
     if dc_crack_model_shear_str=="ModeII_throughcrack_CSDformula" and dc_symmetric_cod_bool is None:
         raise ValueError("dc:symmetric_cod must be set to 'true' or 'false' when using ModeII_throughcrack_CSDformula crack model")
-    elif dc_crack_model_shear_str=="Fabrikant_ModeII_CircularCrack_along_midline":
+    elif dc_crack_model_shear_str=="Fabrikant_ModeII_CircularCrack_along_midline" and dc_symmetric_cod_bool is None:
         # Fabrikant model inherently symmetric also
         dc_symmetric_cod_bool=True
         pass
